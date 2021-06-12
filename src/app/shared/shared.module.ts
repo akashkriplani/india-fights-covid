107933c41bb1206b14217c99799a6bcc060d9daa
Akash Kriplani <aakash23kriplani@gmail.com>
2021-06-12 16:39:04 +0530

    IFC: Add appointment table part 1

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,4 +1,6 @@
 import { ModuleWithProviders, NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -7,12 +9,12 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatSelectModule } from '@angular/material/select';
+import { MatTableModule } from '@angular/material/table';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { CounterDirective } from './directives/counter.directive';
+import { AppointmentTableComponent } from './components/appointment-table/appointment-table.component';
 import { OtpComponent } from './components/otp/otp.component';
-import { ReactiveFormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
+import { CounterDirective } from './directives/counter.directive';
 import { MaskPhonePipe } from './pipes/mask-phone.pipe';
 import { FormatPhonePipe } from './pipes/format-phone.pipe';
 
@@ -25,11 +27,13 @@ const materialModules = [
   MatInputModule,
   MatProgressSpinnerModule,
   MatSelectModule,
+  MatTableModule,
   MatTabsModule,
   MatToolbarModule
 ];
 
 const components = [
+  AppointmentTableComponent,
   OtpComponent
 ];
 
