af9f074038893b8aa48454083ff97ad849089f61
Akash Kriplani <aakash23kriplani@gmail.com>
2021-06-13 17:35:37 +0530

    IFC: Add basic filtering

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,6 +2,7 @@ import { ModuleWithProviders, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatGridListModule } from '@angular/material/grid-list';
@@ -23,6 +24,7 @@ import { SafePipe } from './pipes/safe.pipe';
 
 const materialModules = [
   MatButtonModule,
+  MatButtonToggleModule,
   MatCardModule,
   MatFormFieldModule,
   MatGridListModule,
